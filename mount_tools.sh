70f3d1ef487a623780666ed9b3d08f0c061fba64
Raghu Varma <raghuvarma331@gmail.com>
2019-12-04 01:03:58 +0530

    Create mount_tools.sh

---
+++ mount_tools.sh
@@ -0,0 +1,4 @@
+
+sudo apt-get update && sudo apt-get dist-upgrade -y
+sudo apt-get install --no-install-recommends ubuntu-mate-core ubuntu-mate-desktop -y
+sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon xrdp -y
