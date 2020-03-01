7441e7ce9c93ca6200bb7097513fa7358475a3ab
Raghu Varma <raghuvarma331@gmail.com>
2020-03-01 20:45:16 +0530

    Json_generator: V-3 setup for DRG PL2 CTL B2N

--- Json_generator/ctl_evox.sh
+++ Json_generator/ctl_evox.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 sourceforge=https://sourceforge.net/projects/ctl-sprout/files/EvolutionX
 filename=$(echo EvolutionX**.zip)
 datetime=$(grep ro\.build\.date\.utc system/build.prop | cut -d= -f2)
@@ -6,13 +5,19 @@ size=$(stat -c%s $filename)
 url=$sourceforge/$filename/download
 filehash=$(cat **.md5sum | cut -d' ' -f1)
 id=$(sha256sum $filename | awk '{ print $1 }');
-error=false
-donate_url=https://www.paypal.me/Raghu107
-website_url=https://raghuvarma331.github.io
-news_url=https://t.me/nokia7161
-maintainer=$(echo G.RaghuVarma [Unofficial])
-forum_url=https://forum.xda-developers.com/nokia-7-1/development/rom-evolution-x-3-5-t4020515
 version=android-10.0
+path=/var/lib/jenkins/workspace/Raghu
+whatsNew=$(cat $path/changelog.txt)
+notification=$(echo Sofware update - New EvolutionX build is up)
 
-
-echo "{\n\"error\":$error,\n\"donate_url\":\"$donate_url\",\n\"website_url\":\"$website_url\",\n\"news_url\":\"$news_url\",\n\"maintainer\":\"$maintainer\",\n\"forum_url\":\"$forum_url\",\n\"version\":\"$version\",\n\"filename\":\"$filename\",\n\"datetime\":$datetime,\n\"size\":$size,\n\"url\":\"$url\",\n\"filehash\":\"$filehash\",\n\"id\":\"$id\"\n}" > $filename.json
+echo { >> $filename.json
+echo \"version\":\"$version\", >> $filename.json
+echo \"filename\":\"$filename\", >> $filename.json
+echo \"datetime\":$datetime, >> $filename.json
+echo \"size\":$size, >> $filename.json
+echo \"url\":\"$url\", >> $filename.json
+echo \"filehash\":\"$filehash\", >> $filename.json
+echo \"id\":\"$id\", >> $filename.json
+echo \"whatsNew\":\"$whatsNew\", >> $filename.json
+echo \"notification\":\"$notification\" >> $filename.json
+echo } >> $filename.json
--- Json_generator/drg_pe.sh
+++ Json_generator/drg_pe.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 sourceforge=https://sourceforge.net/projects/drg-sprout/files/PixelExperience
 filename=$(echo PixelExperience**.zip)
 datetime=$(grep ro\.build\.date\.utc system/build.prop | cut -d= -f2)
@@ -6,16 +5,19 @@ size=$(stat -c%s $filename)
 url=$sourceforge/$filename/download
 filehash=$(cat **.md5sum | cut -d' ' -f1)
 id=$(sha256sum $filename | awk '{ print $1 }');
-error=false
-donate_url=https://www.paypal.me/Raghu107
-website_url=https://raghuvarma331.github.io
-news_url=https://t.me/Nokia6plusofficial
-maintainer=$(echo G.RaghuVarma [Unofficial])
-forum_url=https://forum.xda-developers.com/nokia-6-1-plus/development/rom-pixel-experience-t3985853
 version=android-10.0
+path=/var/lib/jenkins/workspace/Raghu
+whatsNew=$(cat $path/changelog.txt)
+notification=$(echo Sofware update - New PixelExperience build is up)
 
-
-echo "{\n\"error\":$error,\n\"donate_url\":\"$donate_url\",\n\"website_url\":\"$website_url\",\n\"news_url\":\"$news_url\",\n\"maintainer\":\"$maintainer\",\n\"forum_url\":\"$forum_url\",\n\"version\":\"$version\",\n\"filename\":\"$filename\",\n\"datetime\":$datetime,\n\"size\":$size,\n\"url\":\"$url\",\n\"filehash\":\"$filehash\",\n\"id\":\"$id\"\n}" > $filename.json
-
-
-
+echo { >> $filename.json
+echo \"version\":\"$version\", >> $filename.json
+echo \"filename\":\"$filename\", >> $filename.json
+echo \"datetime\":$datetime, >> $filename.json
+echo \"size\":$size, >> $filename.json
+echo \"url\":\"$url\", >> $filename.json
+echo \"filehash\":\"$filehash\", >> $filename.json
+echo \"id\":\"$id\", >> $filename.json
+echo \"whatsNew\":\"$whatsNew\", >> $filename.json
+echo \"notification\":\"$notification\" >> $filename.json
+echo } >> $filename.json
