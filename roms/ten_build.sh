932df823c4b6dbd13d5272054ef5c2b8ffdecb09
Raghu Varma <raghuvarma331@gmail.com>
2020-03-01 19:21:48 +0530

    we welcome our brand new ota application
    thanks to my contributor for pulling this project on time

--- roms/ten_build.sh
+++ roms/ten_build.sh
@@ -48,7 +48,7 @@ LINEAGE-SOURCE()
     sed -i "/ro.control_privapp_permissions=enforce/d" vendor/lineage/config/common.mk
     rm -r packages/apps/Settings
     rm -r packages/apps/Updater
-    git clone https://github.com/RaghuVarma331/Os_Updates.git -b pe-ten packages/apps/Os_Updates    
+    git clone https://$gitpassword@github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates    
     git clone https://github.com/LineageOS/android_packages_apps_Settings.git -b lineage-17.1 packages/apps/Settings
     cd packages/apps/Settings
     git remote add main https://github.com/RaghuVarma331/settings.git
@@ -364,7 +364,7 @@ PE-SOURCE()
     sed -i "/ro.control_privapp_permissions=enforce/d" vendor/aosp/config/common.mk
     rm -r packages/apps/Settings
     rm -r packages/apps/Updates
-    git clone https://github.com/RaghuVarma331/Os_Updates.git -b pe-ten packages/apps/Os_Updates       
+    git clone https://$gitpassword@github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates         
     git clone https://github.com/PixelExperience/packages_apps_Settings.git -b ten packages/apps/Settings
     cd packages/apps/Settings    
     git remote add main https://github.com/RaghuVarma331/settings.git
@@ -620,7 +620,7 @@ EXT-SOURCE()
     sed -i "/ro.control_privapp_permissions=enforce/d" vendor/aosp/config/common.mk
     rm -r packages/apps/Settings
     rm -r packages/apps/Updates
-    git clone https://github.com/RaghuVarma331/Os_Updates.git -b pe-ten packages/apps/Os_Updates       
+    git clone https://$gitpassword@github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates    
     git clone https://github.com/Extended-UI/android_packages_apps_Settings.git -b android_10 packages/apps/Settings
     cd packages/apps/Settings
     git remote add main https://github.com/RaghuVarma331/settings.git
@@ -944,7 +944,7 @@ EVOX-SOURCE()
     sed -i "/ro.control_privapp_permissions=enforce/d" vendor/aosp/config/common.mk
     rm -r packages/apps/Settings
     rm -r packages/apps/Updates
-    git clone https://github.com/RaghuVarma331/Os_Updates.git -b pe-ten packages/apps/Os_Updates       
+    git clone https://$gitpassword@github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates  
     git clone https://github.com/Evolution-X/packages_apps_Settings.git -b ten packages/apps/Settings
     cd packages/apps/Settings
     git remote add main https://github.com/RaghuVarma331/settings.git
