1cbde34084a27a5b1f1ed78acd6a8d4eac931fc7
Raghu Varma <raghuvarma331@gmail.com>
2020-04-05 23:56:36 +0530

    ship builds with snapdragon camera

--- roms/ten_build.sh
+++ roms/ten_build.sh
@@ -59,8 +59,6 @@ LINEAGE-SOURCE()
     git clone https://github.com/RaghuVarma331/android_device_nokia_Dragon.git -b ten-enforcing device/nokia/Dragon 
     git clone https://github.com/RaghuVarma331/android_kernel_nokia_sdm660.git -b ten-gcc --depth=1 kernel/nokia/sdm660
     git clone https://gitlab.com/RaghuVarma331/vendor_nokia.git -b ten --depth=1 vendor/nokia
-    git clone https://github.com/RaghuVarma331/vendor_nokia_Camera.git -b ten --depth=1 vendor/nokia/Camera
-    git clone https://gitlab.com/RaghuVarma331/vendor_nokia_hmdcamera.git -b ten --depth=1 vendor/nokia/HMDCamera
     git clone https://gitlab.com/RaghuVarma331/vendor_gapps.git -b ten --depth=1 vendor/gapps
     git clone https://github.com/RaghuVarma331/device_custom_sepolicy.git -b los-ten device/lineage/sepolicy
     git clone https://github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates    
@@ -497,8 +495,6 @@ PE-SOURCE()
     git clone https://github.com/RaghuVarma331/android_device_nokia_Dragon.git -b ten-enforcing device/nokia/Dragon 
     git clone https://github.com/RaghuVarma331/android_kernel_nokia_sdm660.git -b ten-gcc --depth=1 kernel/nokia/sdm660
     git clone https://gitlab.com/RaghuVarma331/vendor_nokia.git -b ten --depth=1 vendor/nokia
-    git clone https://github.com/RaghuVarma331/vendor_nokia_Camera.git -b ten --depth=1 vendor/nokia/Camera
-    git clone https://gitlab.com/RaghuVarma331/vendor_nokia_hmdcamera.git -b ten --depth=1 vendor/nokia/HMDCamera
     git clone https://gitlab.com/RaghuVarma331/vendor_gapps.git -b ten --depth=1 vendor/gapps
     git clone https://github.com/RaghuVarma331/device_custom_sepolicy.git -b pe-ten device/custom/sepolicy
     git clone https://github.com/RaghuVarma331/android_packages_apps_Gallery2.git -b lineage-17.1 packages/apps/Gallery2
@@ -855,8 +851,6 @@ EVOX-SOURCE()
     git clone https://github.com/RaghuVarma331/android_device_nokia_Dragon.git -b ten-permissive device/nokia/Dragon 
     git clone https://github.com/RaghuVarma331/android_kernel_nokia_sdm660.git -b ten-gcc --depth=1 kernel/nokia/sdm660
     git clone https://gitlab.com/RaghuVarma331/vendor_nokia.git -b ten --depth=1 vendor/nokia
-    git clone https://github.com/RaghuVarma331/vendor_nokia_Camera.git -b ten --depth=1 vendor/nokia/Camera
-    git clone https://gitlab.com/RaghuVarma331/vendor_nokia_hmdcamera.git -b ten --depth=1 vendor/nokia/HMDCamera
     git clone https://gitlab.com/RaghuVarma331/vendor_gapps.git -b ten --depth=1 vendor/gapps
     git clone https://github.com/RaghuVarma331/device_custom_sepolicy.git -b pe-ten device/custom/sepolicy
     git clone https://github.com/RaghuVarma331/android_packages_apps_Gallery2.git -b lineage-17.1 packages/apps/Gallery2
