57cafdd35e5129afcab0f48cb088d6b12f6d3cb3
RaghuVarma331 <raghuvarma331@gmail.com>
2020-04-16 19:27:07 +0000

    Drop evolution again
    Reason - no proper Selinux support and also no proper maintenance from them. Sometimes rom will boot & sometimes doesn't so over all I am done with evox and also from my end no issues
    
    Also requested to stalix ( lead developer )  to maintain Gerrit and merge at weekends but no response

--- roms/ten_build.sh
+++ roms/ten_build.sh
@@ -24,7 +24,6 @@ path=/var/lib/jenkins/workspace/Raghu
 securitypatch=2020-04-05
 twrpsp='2020-04-05'
 oxversion=v-7
-evoxversion=4.1.169
 tag=4.4.194
 jenkinsurl=
 
@@ -586,311 +585,6 @@ PE-SOURCE()
     rm -r pe    
 }
 
-EVOX-SOURCE()
-{
-    git clone https://$gitpassword@github.com/RaghuVarma331/Keys keys
-    wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
-    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/evox.png
-    wget https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/crossdevelopment/changelog.txt
-    git clone https://$gitpassword@github.com/RaghuVarma331/Json-Tracker.git json 
-    git clone https://github.com/RaghuVarma331/prebuilt_kernels.git -b ten prebuilts    
-    mkdir evo
-    cd evo
-    echo -ne '\n' | repo init -u https://github.com/Evolution-X/manifest -b ten --depth=1
-    repo sync -c --no-tags --no-clone-bundle -f --force-sync -j16
-    sed -i "/ro.control_privapp_permissions=enforce/d" vendor/aosp/config/common.mk
-    rm -r packages/apps/Gallery2
-    rm -r vendor/gapps
-    rm -r packages/apps/Settings
-    rm -r packages/apps/Updates
-    rm -r device/custom/sepolicy 
-    git clone https://github.com/RaghuVarma331/android_device_nokia_Dragon.git -b ten-permissive device/nokia/Dragon 
-    git clone https://github.com/RaghuVarma331/android_kernel_nokia_sdm660.git -b ten-gcc --depth=1 kernel/nokia/sdm660
-    git clone https://github.com/RaghuVarma331/android_vendor_nokia.git -b ten vendor/nokia
-    git clone https://gitlab.com/RaghuVarma331/vendor_gapps.git -b ten --depth=1 vendor/gapps
-    git clone https://github.com/RaghuVarma331/device_custom_sepolicy.git -b pe-ten device/custom/sepolicy
-    git clone https://github.com/RaghuVarma331/android_packages_apps_Gallery2.git -b lineage-17.1 packages/apps/Gallery2
-    git clone https://github.com/RaghuVarma331/Os_Updates.git -b pixel-ten packages/apps/Os_Updates          
-    git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.1 packages/resources/devicesettings 
-    git clone https://github.com/Evolution-X/packages_apps_Settings.git -b ten packages/apps/Settings
-    cd packages/apps/Settings    
-    git remote add main https://github.com/RaghuVarma331/settings.git
-    git fetch main
-    git cherry-pick d0dede567168181d4f0035f61cf12f2996445be7
-    sed -i "/<<<<<<< HEAD/d" res/xml/firmware_version.xml
-    sed -i "/=======/d" res/xml/firmware_version.xml
-    sed -i "/>>>>>>>/d" res/xml/firmware_version.xml
-    cd src/com/android/settings/system
-    rm -r SystemUpdatePreferenceController.java
-    wget https://raw.githubusercontent.com/RaghuVarma331/settings/ten/src/com/android/settings/system/SystemUpdatePreferenceController.java
-    cd
-    cd $path/evo
-    cd packages/apps/Os_Updates/src/org/pixelexperience/ota/misc
-    rm -r Constants.java
-    wget https://github.com/RaghuVarma331/Json-configs/raw/master/Dragon/Evolution/Constants.java
-    cd
-    cd $path/evo
-    curl -s -X POST https://api.telegram.org/bot$Telegram_Api_code/sendMessage -d chat_id=$chat_id -d text="
-    
-    New Evolution X for Nokia 6.1 Plus build started 
-    
-    $(date)
-    
-    👤 By: Raghu Varma
-    build's progress at $jenkinsurl"      
-    export SKIP_ABI_CHECKS=true
-    . build/envsetup.sh && lunch aosp_Dragon-userdebug && make -j32 bacon
-    cd out/target/product/Dragon
-    rm -r **.json
-    wget https://github.com/RaghuVarma331/scripts/raw/master/Json_generator/drg_evox.sh
-    chmod a+x drg_evox.sh
-    ./drg_evox.sh
-    zipname=$(echo EvolutionX**.zip)
-    cat $zipname.json > $path/json/Dragon/evox.json     
-    sshpass -p $password rsync -avP -e ssh EvolutionX**.zip       raghuvarma331@frs.sourceforge.net:/home/frs/project/drg-sprout/EvolutionX
-    cd 
-    cd $path
-    python telegram.py -t $Telegram_Api_code -c $chat_id  -P evox.png -C "
-    *
-    New Evolution X Build is up 
-    
-    $(date)*
-    
-    ⬇️ [Download ROM](https://forum.xda-developers.com/nokia-6-1-plus/development/rom-evolution-x-3-3-t4011589)
-    ⬇️ [Download Vendor](https://forum.xda-developers.com/nokia-6-1-plus/development/vendor-drg-drgsprout-treble-gsi-vendor-t4040201)
-    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
-    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
-    📱Device: *Nokia 6.1 Plus*
-    ⚡Build Version: *$evoxversion*
-    ⚡Android Version: *10.0.0*
-    ⚡Security Patch : *$securitypatch*
-    👤 By: *Raghu Varma*
-    #drg #nokia #evox #update
-    Follow:  @Nokia6plusofficial ✅"  
-    cd json
-    git add .
-    git commit -m "Dragon: Evolution-X 10.0 build $(date)"
-    git push -u -f origin master
-    cd ..     
-    cd evo
-    rm -r device/nokia
-    rm -r out/target/product/**
-    git clone https://github.com/RaghuVarma331/android_device_nokia_Onyx.git -b ten-permissive device/nokia/Onyx  
-    cd packages/apps/Os_Updates/src/org/pixelexperience/ota/misc
-    rm -r Constants.java
-    wget https://github.com/RaghuVarma331/Json-configs/raw/master/Onyx/Evolution/Constants.java
-    cd
-    cd $path/evo
-    curl -s -X POST https://api.telegram.org/bot$Telegram_Api_code/sendMessage -d chat_id=$chat_id -d text="
-    
-    New Evolution X for Nokia 7 Plus build started 
-    
-    $(date)
-    
-    👤 By: Raghu Varma
-    build's progress at $jenkinsurl"      
-    export SKIP_ABI_CHECKS=true
-    . build/envsetup.sh && lunch aosp_Onyx-userdebug && make -j32 bacon
-    cd out/target/product/Onyx
-    rm -r **.json
-    wget https://github.com/RaghuVarma331/scripts/raw/master/Json_generator/b2n_evox.sh
-    chmod a+x b2n_evox.sh
-    ./b2n_evox.sh
-    zipname=$(echo EvolutionX**.zip)
-    cat $zipname.json > $path/json/Onyx/evox.json     
-    sshpass -p $password rsync -avP -e ssh EvolutionX**.zip  raghuvarma331@frs.sourceforge.net:/home/frs/project/b2n-sprout/EvolutionX
-    cd 
-    cd $path
-    python telegram.py -t $Telegram_Api_code -c $chat_id  -P evox.png -C "
-    *
-    New Evolution X Build is up 
-    
-    $(date)*
-     
-    ⬇️ [Download ROM](https://forum.xda-developers.com/nokia-7-plus/development/rom-evolution-x-3-3-t4011603)
-    ⬇️ [Download Vendor](https://forum.xda-developers.com/nokia-7-plus/development/vendor-b2n-b2nsprout-treble-gsi-vendor-t4040207)
-    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
-    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
-    📱Device: *Nokia 7 Plus*
-    ⚡Build Version: *$evoxversion*
-    ⚡Android Version: *10.0.0*
-    ⚡Security Patch : *$securitypatch*
-    👤 By: *Raghu Varma*
-    #b2n #nokia #evox #update
-    Follow: @Nokia7plusOfficial ✅"  
-    cd json
-    git add .
-    git commit -m "Onyx: Evolution-X 10.0 build $(date)"
-    git push -u -f origin master
-    cd ..     
-    cd evo
-    rm -r device/nokia
-    rm -r out/target/product/**
-    git clone https://github.com/RaghuVarma331/android_device_nokia_Crystal.git -b ten-permissive device/nokia/Crystal   
-    cd packages/apps/Os_Updates/src/org/pixelexperience/ota/misc
-    rm -r Constants.java
-    wget https://github.com/RaghuVarma331/Json-configs/raw/master/Crystal/Evolution/Constants.java
-    cd
-    cd $path/evo 
-    curl -s -X POST https://api.telegram.org/bot$Telegram_Api_code/sendMessage -d chat_id=$chat_id -d text="
-    
-    New Evolution X for Nokia 7.1 build started 
-    
-    $(date)
-    
-    👤 By: Raghu Varma
-    build's progress at $jenkinsurl"      
-    export SKIP_ABI_CHECKS=true
-    . build/envsetup.sh && lunch aosp_Crystal-userdebug && make -j32 bacon
-    cd out/target/product/Crystal
-    rm -r **.json
-    wget https://github.com/RaghuVarma331/scripts/raw/master/Json_generator/ctl_evox.sh
-    chmod a+x ctl_evox.sh
-    ./ctl_evox.sh
-    zipname=$(echo EvolutionX**.zip)
-    cat $zipname.json > $path/json/Crystal/evox.json        
-    sshpass -p $password rsync -avP -e ssh EvolutionX**.zip       raghuvarma331@frs.sourceforge.net:/home/frs/project/ctl-sprout/EvolutionX
-    cd 
-    cd $path
-    python telegram.py -t $Telegram_Api_code -c $chat_id  -P evox.png -C "
-    *
-    New Evolution X Build is up 
-    
-    $(date)*
-     
-    ⬇️ [Download ROM](https://forum.xda-developers.com/nokia-7-1/development/rom-evolution-x-3-5-t4020515)
-    ⬇️ [Download Vendor](https://forum.xda-developers.com/nokia-7-1/development/vendor-ctl-ctlsprout-treble-gsi-vendor-t4040211)
-    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
-    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
-    📱Device: *Nokia 7.1*
-    ⚡Build Version: *$evoxversion*
-    ⚡Android Version: *10.0.0*
-    ⚡Security Patch : *$securitypatch*
-    👤 By: *Raghu Varma*
-    #ctl #nokia #evox #update
-    Follow: @nokia7161 ✅"      
-    cd json
-    git add .
-    git commit -m "Crystal: Evolution-X 10.0 build $(date)"
-    git push -u -f origin master
-    cd ..           
-    cd evo
-    rm -r device/nokia
-    rm -r out/target/product/**
-    git clone https://github.com/RaghuVarma331/android_device_nokia_Daredevil.git -b ten-permissive device/nokia/Daredevil 
-    cd packages/apps/Os_Updates/src/org/pixelexperience/ota/misc
-    rm -r Constants.java
-    wget https://github.com/RaghuVarma331/Json-configs/raw/master/Daredevil/Evolution/Constants.java
-    cd
-    cd $path/evo
-    cd vendor/aosp/build/tasks
-    rm -r kernel.mk
-    wget https://github.com/RaghuVarma331/vendor_custom/raw/ten-los/build/tasks/kernel.mk
-    cd
-    cd $path/evo        
-    curl -s -X POST https://api.telegram.org/bot$Telegram_Api_code/sendMessage -d chat_id=$chat_id -d text="
-    
-    New Evolution X for Nokia 7.2 build started 
-    
-    $(date)
-    
-    👤 By: Raghu Varma
-    build's progress at $jenkinsurl"      
-    export SKIP_ABI_CHECKS=true
-    . build/envsetup.sh && lunch aosp_Daredevil-userdebug && make -j32 bacon
-    cd out/target/product/Daredevil
-    rm -r **.json
-    wget https://github.com/RaghuVarma331/scripts/raw/master/Json_generator/ddv_evox.sh
-    chmod a+x ddv_evox.sh
-    ./ddv_evox.sh
-    zipname=$(echo EvolutionX**.zip)
-    cat $zipname.json > $path/json/Daredevil/evox.json        
-    sshpass -p $password rsync -avP -e ssh EvolutionX**.zip       raghuvarma331@frs.sourceforge.net:/home/frs/project/ddv-sprout/EvolutionX
-    cd 
-    cd $path
-    python telegram.py -t $Telegram_Api_code -c $chat_id  -P evox.png -C "
-    *
-    New Evolution X Build is up 
-    
-    $(date)*
-     
-    ⬇️ [Download ROM](https://forum.xda-developers.com/nokia-7-2/development/rom-evolution-x-4-1-169-t4077205)
-    ⬇️ [Download Vendor](https://forum.xda-developers.com/nokia-7-2/development/vendor-ddv-ddvsprout-treble-gsi-vendor-t4083095)
-    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
-    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
-    📱Device: *Nokia 7.2*
-    ⚡Build Version: *$evoxversion*
-    ⚡Android Version: *10.0.0*
-    ⚡Security Patch : *$securitypatch*
-    👤 By: *Raghu Varma*
-    #ddv #nokia #evox #update
-    Follow: @Nokia7262 ✅"      
-    cd json
-    git add .
-    git commit -m "Daredevil: Evolution-X 10.0 build $(date)"
-    git push -u -f origin master
-    cd ..           
-    cd evo    
-    rm -r device/nokia
-    rm -r out/target/product/**
-    git clone https://github.com/RaghuVarma331/android_device_nokia_Plate2.git -b ten-permissive device/nokia/Plate2
-    cd packages/apps/Os_Updates/src/org/pixelexperience/ota/misc
-    rm -r Constants.java
-    wget https://github.com/RaghuVarma331/Json-configs/raw/master/Plate2/Evolution/Constants.java
-    cd
-    cd $path/evo
-    cd vendor/aosp/build/tasks
-    rm -r kernel.mk
-    wget https://github.com/Evolution-X/vendor_aosp/raw/ten/build/tasks/kernel.mk
-    cd
-    cd $path/evo      
-    cd
-    cd $path/evo   
-    curl -s -X POST https://api.telegram.org/bot$Telegram_Api_code/sendMessage -d chat_id=$chat_id -d text="
-    
-    New Evolution X for Nokia 6.1 build started 
-    
-    $(date)
-    
-    👤 By: Raghu Varma
-    build's progress at $jenkinsurl" 
-    export SKIP_ABI_CHECKS=true
-    . build/envsetup.sh && lunch aosp_Plate2-userdebug && make -j32 bacon
-    cd out/target/product/Plate2
-    rm -r **.json
-    wget https://github.com/RaghuVarma331/scripts/raw/master/Json_generator/pl2_evox.sh
-    chmod a+x pl2_evox.sh
-    ./pl2_evox.sh
-    zipname=$(echo EvolutionX**.zip)
-    cat $zipname.json > $path/json/Plate2/evox.json     
-    sshpass -p $password rsync -avP -e ssh EvolutionX**.zip  raghuvarma331@frs.sourceforge.net:/home/frs/project/pl2-sprout/EvolutionX
-    cd 
-    cd $path
-    rm -r evo
-    python telegram.py -t $Telegram_Api_code -c $chat_id  -P evox.png -C "
-    *
-    New Evolution X Build is up 
-    
-    $(date)*
-    
-    ⬇️ [Download ROM](https://forum.xda-developers.com/nokia-6-2018/development/rom-evolution-x-3-3-t4011611)
-    ⬇️ [Download Vendor](https://forum.xda-developers.com/nokia-6-2018/development/vendor-pl2-pl2sprout-treble-gsi-vendor-t4040213)
-    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
-    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
-    📱Device: *Nokia 6.1*
-    ⚡Build Version: *$evoxversion*
-    ⚡Android Version: *10.0.0*
-    ⚡Security Patch : *$securitypatch*
-    👤 By: *Raghu Varma*
-    #pl2 #nokia #evox #update
-    Follow: @nokia7161 ✅"          
-    cd json
-    git add .
-    git commit -m "Plate2: Evolution-X 10.0 build $(date)"
-    git push -u -f origin master
-    cd ..         
-}
-
 TWRP-P-SOURCE()
 {
     wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
@@ -1416,8 +1110,8 @@ echo "------------------------------------------------"
 echo "A simple remote script to compile custom Stuff"
 echo "Coded By Raghu Varma.G "
 echo "------------------------------------------------"
-PS3='Please select your option (1-8): '
-menuvar=("BasicSetup" "pe" "lineageos" "evox" "oxygen" "twrp" "Black_Caps-Edition" "all_roms" "Exit")
+PS3='Please select your option (1-7): '
+menuvar=("BasicSetup" "pe" "lineageos" "oxygen" "twrp" "Black_Caps-Edition" "all_roms" "Exit")
 select menuvar in "${menuvar[@]}"
 do
     case $menuvar in
@@ -1571,36 +1265,6 @@ do
             read -n1 -r key
             break
             ;;
-        "evox")
-            clear
-            echo "----------------------------------------------"
-            echo "Started Building Evox for Nokia 6.1 Plus , 7 Plus & 6.1  ."
-            echo "Please be patient..."
-            # evox
-            echo "----------------------------------------------"
-            echo "Repo Setup..."
-            echo " "
-            REPO
-	    echo " " 	    
-            echo "----------------------------------------------"
-            echo "Setting Up Tools & Stuff..."
-            echo " "
-            TOOLS_SETUP
-	    echo " "	    
-            echo "----------------------------------------------"
-            echo "Setting up evox source..."
-            echo " "
-            EVOX-SOURCE
-	    echo " "	 	    
-            echo "----------------------------------------------"
-            echo "Build successfully completed."
-            echo " "
-            echo "----------------------------------------------"
-            echo "Press any key for end the script."
-            echo "----------------------------------------------"
-            read -n1 -r key
-            break
-            ;;		    
         "oxygen")
             clear
             echo "----------------------------------------------"
@@ -1629,7 +1293,7 @@ do
         "all_roms")
             clear
             echo "----------------------------------------------"
-            echo "Started Building LineageOS 17.1 , Pixel-EXP , Evox & Twrp for Nokia 6.1 Plus , 7 plus , 6.1 , 6.2 & 7.2 ."
+            echo "Started Building LineageOS 17.1 , Pixel-EXP & Twrp for Nokia 6.1 Plus , 7 plus , 6.1 , 6.2 & 7.2 ."
             echo "Please be patient..."
             # all_roms
             echo "----------------------------------------------"
@@ -1652,11 +1316,6 @@ do
             echo " "
             PE-SOURCE
 	    echo " "
-            echo "----------------------------------------------"
-            echo "Setting up evox source..."
-            echo " "
-            EVOX-SOURCE
-	    echo " "	    
             echo "----------------------------------------------"
             echo "Setting up OxygenOS source..."
             echo " "
