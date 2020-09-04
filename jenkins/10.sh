28484d886ff9b0b072d690de92f6e53e76ebeba8
Raghu Varma <raghuvarma331@gmail.com>
2020-09-04 12:48:13 +0530

    Jenkins: Restart immediately after increasing timeout

--- jenkins/10.sh
+++ jenkins/10.sh
@@ -5,3 +5,4 @@ path=$(sudo find / -name pluginSetupWizard.js)
 sudo systemctl start jenkins
 echo "// default 10 seconds for AJAX responses to return before triggering an error condition" >> $path
 echo "var pluginManagerErrorTimeoutMillis = 180 * 1000;" >> $path
+sudo service jenkins restart
