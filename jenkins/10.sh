72ef3be555363aac1a7bb631dc9e8014b6836d41
Raghu Varma <raghuvarma331@gmail.com>
2020-09-04 12:41:14 +0530

    Jenkins: Setup wizard timeout for "Install Plugins"

--- jenkins/10.sh
+++ jenkins/10.sh
@@ -1,2 +1,7 @@
- sudo systemctl start jenkins
+#!/bin/bash
 
+path=$(sudo find / -name pluginSetupWizard.js)
+
+sudo systemctl start jenkins
+echo "// default 10 seconds for AJAX responses to return before triggering an error condition" >> $path
+echo "var pluginManagerErrorTimeoutMillis = 180 * 1000;" >> $path
