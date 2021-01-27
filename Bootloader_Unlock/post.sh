f3451423912db33cc9358d7b653ad95566319e23
RaghuVarma331 <raghuvarma331@gmail.com>
2021-01-27 05:58:21 +0000

    Bootloader_Unlock: initiate bootloader unlocking support
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

---
+++ Bootloader_Unlock/post.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# RAGHU VARMA Build Script 
+# Coded by RV 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+Telegram_Api_code=
+
+wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
+
+python telegram.py -t $Telegram_Api_code -c @Nokia6plusofficial -M "
+*
+Bootloader unlocking support
+=======================
+
+Planning to unlock bootloader ?
+
+Let me know @RaghuVarma 🙂
+
+Thank you
+*
+"
+
+python telegram.py -t $Telegram_Api_code -c @Nokia7plusOfficial -M "
+*
+Bootloader unlocking support
+=======================
+
+Planning to unlock bootloader ?
+
+Let me know @RaghuVarma 🙂
+
+Thank you
+*
+"
+
+python telegram.py -t $Telegram_Api_code -c @Nokia7262 -M "
+*
+Bootloader unlocking support
+=======================
+
+Planning to unlock bootloader ?
+
+Let me know @RaghuVarma 🙂
+
+Thank you
+*
+"
+
+python telegram.py -t $Telegram_Api_code -c @nokia7161 -M "
+*
+Bootloader unlocking support
+=======================
+
+Planning to unlock bootloader ?
+
+Let me know @RaghuVarma 🙂
+
+Thank you
+*
+"
+
