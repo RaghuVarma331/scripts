97db023a4d0e8cbf4007a4048ce68a1897a1c859
Raghu Varma <raghuvarma331@gmail.com>
2021-11-22 12:08:33 +0530

    HMD-AOSP: add Governor OS telegram post update script

---
+++ Nokia-SDM660/HMD-AOSP.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+path=/root
+securitypatch=2021-11
+Telegram_Api_code=
+chat_id=
+
+    wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
+    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/aosp-12.png
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.1 Plus*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #drg #nokia #gos #update
+    Follow:  @Nokia6plusofficial ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7 Plus*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #b2n #nokia #gos #update
+    Follow:  @Nokia7plusofficial ✅" 
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.1*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ctl #nokia #gos #update
+    Follow:  @nokia7161 ✅"   
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.1*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #pl2 #nokia #gos #update
+    Follow:  @nokia7161 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.2*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ddv #nokia #gos #update
+    Follow:  @Nokia7262 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P aosp-12.png -C "
+    *
+    🔥  New GovernorOS Build
+    ==========================
+    $(date)*
+    
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.2*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #sld #nokia #gos #update
+    Follow:  @Nokia7262 ✅"  
