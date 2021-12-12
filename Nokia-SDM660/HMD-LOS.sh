ca553e39ddcfdb347021c2b8de7133e8edecc31d
Raghu Varma <raghuvarma331@gmail.com>
2021-12-12 12:17:58 +0530

    HMD-LOS: add LineageOS telegram post update script

---
+++ Nokia-SDM660/HMD-LOS.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+path=/root
+securitypatch=2021-11
+Telegram_Api_code=
+chat_id=
+
+
+    wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
+    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/lineage.jpg
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
+    *
+    🔥 New LineageOS 19.0 Build
+    ==========================
+    $(date)*
+
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)    
+    ⬇️ [Download Rom](https://forum.xda-developers.com/t/rom-12-0-dragon-lineageos-19-0-unofficial.3985367/)
+    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.1 Plus*
+    ⚡Build Version: *19.0* 
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #drg #nokia #los #update
+    Follow:  @Nokia6plusofficial ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
+    *
+    🔥 New LineageOS 19.0 Build
+    ==========================    
+    $(date)*
+    
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download ROM](https://forum.xda-developers.com/t/rom-12-0-onyx-lineageos-19-0-unofficial.3993445/)
+    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7 Plus*
+    ⚡Build Version: *19.0* 
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #b2n #nokia #los #update
+    Follow: @Nokia7plusOfficial ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
+    *
+    🔥 New LineageOS 19.0 Build
+    ==========================    
+    $(date)*
+
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)    
+    ⬇️ [Download ROM](https://forum.xda-developers.com/t/rom-12-0-crystal-lineageos-19-0-unofficial.4019915/)
+    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.1*
+    ⚡Build Version: *19.0* 
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ctl #nokia #los #update
+    Follow: @nokia7161 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
+    *
+    🔥 New LineageOS 19.0 Build
+    ==========================    
+    $(date)*
+
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)    
+    ⬇️ [Download ROM](https://forum.xda-developers.com/t/rom-12-0-daredevil-lineageos-19-0-unofficial.4001281/)
+    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.2*
+    ⚡Build Version: *19.0* 
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ddv #nokia #los #update
+    Follow: @Nokia7262 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
+    *
+    🔥 New LineageOS 19.0 Build
+    ==========================    
+    $(date)*
+
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)    
+    ⬇️ [Download ROM](https://forum.xda-developers.com/t/rom-12-0-starlord-lineageos-19-0-unofficial.4001273/)
+    💬 [Device Changelog](https://raw.githubusercontent.com/RaghuVarma331/changelogs/master/nokia.txt)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.2*
+    ⚡Build Version: *19.0* 
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #sld #nokia #los #update
+    Follow: @Nokia7262 ✅"  
+
+
+
