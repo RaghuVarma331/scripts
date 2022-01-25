b4668a0a5139cfd3f1274d32651760d160a0a932
Raghu Varma <raghuvarma331@gmail.com>
2022-01-25 21:20:25 +0530

    HMD-PE: add Pixel Experience telegram post update script

---
+++ Nokia-SDM660/HMD-PE.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+path=/root
+securitypatch=2022-01
+Telegram_Api_code=
+chat_id=
+
+    wget  https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
+    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/pixel.jpg
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.1 Plus*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #drg #nokia #pe #update
+    Follow:  @Nokia6plusofficial ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7 Plus*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #b2n #nokia #pe #update
+    Follow:  @Nokia7plusofficial ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.1*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ctl #nokia #pe #update
+    Follow:  @nokia7161 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.1*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #pl2 #nokia #pe #update
+    Follow:  @nokia7161 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    ⬇️ [Download vbmeta](https://sourceforge.net/projects/sld-sprout/files/vbmeta/TEN/)         
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 7.2*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #ddv #nokia #pe #update
+    Follow:  @Nokia7262 ✅"  
+
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
+    *
+    🔥 New Pixel-Experience Build 
+    ==========================
+    $(date)*
+    🔓 [Bootloader Unlock](https://t.me/RaghuVarma)
+    ⬇️ [Download Rom](https://nokia-sdm660.github.io/)
+    ⬇️ [Download vbmeta](https://sourceforge.net/projects/sld-sprout/files/vbmeta/TEN/)    
+    💬 [Installation procedure](https://github.com/RaghuVarma331/changelogs/raw/master/crossdevelopment/abcrins.txt)
+    📱Device: *Nokia 6.2*
+    ⚡Android Version: *12.0*
+    ⚡Security Patch : *$securitypatch*
+    👤 By: *@RaghuVarma*
+    #sld #nokia #pe #update
+    Follow:  @Nokia7262 ✅"  
