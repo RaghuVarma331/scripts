3aee31138d55446f0a9f324f72296e16da836d76
Raghu Varma <raghuvarma331@gmail.com>
2022-06-10 20:52:20 +0530

    Motorola-SM6150: Add TWRP telegram post script

---
+++ Motorola-SM6150/TWRP-post.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+Telegram_Api_code=
+chat_id=
+version=3.6.2-11.0
+
+
+    wget https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
+    wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/twrp.jpg
+    python telegram.py -t $Telegram_Api_code -c $chat_id  -P twrp.jpg -C "
+    *
+    New Twrp-$version build
+    
+    $(date)*
+    
+    ⬇️ [Download TWRP](https://forum.xda-developers.com/t/unofficial-twrp-3-6-2-11-0-team-win-recovery-project-moto-g60-g40.4453417/)
+    📱Device: *Moto G60/G40*
+    ⚡Build Version: *$version*
+    👤 By: *@RaghuVarma*
+    #g60 #g40 #twrp #update
+    Follow:  @MotoG60G40 ✅" 
+   
