0331f66f51b0c9f55b958f297f3407e3496bb7a4
RaghuVarma331 <raghuvarma331@gmail.com>
2023-01-10 06:58:16 +0000

    Motorola-SM6150: update LineageOS telegram post script
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- Motorola-SM6150/LOS-post.sh
+++ Motorola-SM6150/LOS-post.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 Telegram_Api_code=
-chat_id=
-securitypatch=2022-05
+chat_id=@MotoG60G40
+LV=20.0
+AV=13.0
+securitypatch=2023-01
 
 wget https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
 wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/lineage.jpg
@@ -10,17 +12,16 @@ wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/lineage.jpg
 
 python telegram.py -t $Telegram_Api_code -c $chat_id  -P lineage.jpg -C "
 *$(date)
-    New LineageOS 19.1 Build
+
+    New LineageOS $LV Build
     ================*
     ⬇️ [Download TWRP](https://forum.xda-developers.com/t/unofficial-twrp-3-6-2-11-0-team-win-recovery-project-moto-g60-g40.4453417/)
-    ⬇️ [Download Rom](https://forum.xda-developers.com/t/rom-12-hanoip-lineageos-19-1-aosp-g60-g40.4450789/)
-    💬 [Build changelog](https://github.com/RaghuVarma331/scripts/raw/master/Motorola-SM6150/LOS-changelog.txt)
+    ⬇️ [Download Rom](https://forum.xda-developers.com/t/rom-13-0-hanoip-lineageos-20-0-aosp-g60-g40f.4450789/)
     💬 [Installation procedure](https://github.com/RaghuVarma331/scripts/raw/master/Motorola-SM6150/Installation.txt)
-    📱Device: *Moto G60/G40*
-    ⚡Android Version: *12L*
+    📱Device: *Moto G60/G40F*
+    ⚡Android Version: *$AV*
     ⚡Security Patch : *$securitypatch*
     👤 By: *@RaghuVarma*
-    #g60 #g40 #moto #update
+    #g60 #g40f #los #update
     Follow:  @MotoG60G40 ✅
 "
-
