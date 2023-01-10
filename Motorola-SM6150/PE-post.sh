f26ba68d9fdeddd7f831b3c46b6a747d55d4df33
RaghuVarma331 <raghuvarma331@gmail.com>
2023-01-10 07:00:35 +0000

    Motorola-SM6150: update PixelExperience telegram post script
    Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

--- Motorola-SM6150/PE-post.sh
+++ Motorola-SM6150/PE-post.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 Telegram_Api_code=
-chat_id=
-securitypatch=2022-05
+chat_id=@MotoG60G40
+AV=13.0
+securitypatch=2023-01
 
 wget https://github.com/RaghuVarma331/scripts/raw/master/pythonscripts/telegram.py
 wget https://github.com/RaghuVarma331/custom_roms_banners/raw/master/pixel.jpg
@@ -14,14 +15,12 @@ python telegram.py -t $Telegram_Api_code -c $chat_id  -P pixel.jpg -C "
     New PixelExperience Build
     ================*
     ⬇️ [Download TWRP](https://forum.xda-developers.com/t/unofficial-twrp-3-6-2-11-0-team-win-recovery-project-moto-g60-g40.4453417/)
-    ⬇️ [Download Rom](https://forum.xda-developers.com/t/rom-12-hanoip-pixelexperience-aosp-g60-g40.4449285/)
-    💬 [Build changelog](https://github.com/RaghuVarma331/scripts/raw/master/Motorola-SM6150/PE-changelog.txt)
+    ⬇️ [Download Rom](https://forum.xda-developers.com/t/rom-13-0-hanoip-pixelexperience-aosp-g60-g40f.4449285/)
     💬 [Installation procedure](https://github.com/RaghuVarma331/scripts/raw/master/Motorola-SM6150/Installation.txt)
-    📱Device: *Moto G60/G40*
-    ⚡Android Version: *12L*
+    📱Device: *Moto G60/G40F*
+    ⚡Android Version: *$AV*
     ⚡Security Patch : *$securitypatch*
     👤 By: *@RaghuVarma*
-    #g60 #g40 #moto #update
+    #g60 #g40f #pe #update
     Follow:  @MotoG60G40 ✅
 "
-
